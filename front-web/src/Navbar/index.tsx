eae49b0284f80ab96b7767fc6cdd097bfaaa9d0b
brmgf <a16010@bcc.unifal-mg.edu.br>
2021-01-06 16:59:59 -0300

    Add navbar

---
+++ front-web/src/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import './style.css'
+
+function Navbar() {
+    return (
+        <nav className="main-navbar">
+            <a href="index.tsx">DS Deliver</a>
+        </nav>
+    )
+}
+
+export default Navbar;
